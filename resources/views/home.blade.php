9f4720900819be70a4355cde5893686070a34bd7
NicholasConti <conti94@live.it>
2023-05-05 10:48:46 +0200

    add layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-         @vite('resources/js/app.js')
-    </head>
-    <body>
-        <h1>MY MOVIES</h1>
-        <div class="row">
-           @foreach ($movies as $movie)  {{--EACH MOVIE DB --}}
-            <div class="card" style="width: 18rem;">
-                <a href="{{route('movie', $movie->id)}}" class="text-decoration-none">
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Title: {{ $movie->title}}</li>
-                    <li class="list-group-item">OG Title: {{ $movie->original_title}}</li>
-                    <li class="list-group-item">Nationality: {{ $movie->nationality}}</li>
-                    <li class="list-group-item">Date: {{ $movie->date}}</li>
-                    <li class="list-group-item">Vote: {{ $movie->vote}}</li>
-                  </ul>
-                </a>
-              </div>
-            
-            @endforeach
-        </div>        
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('title')
+    HOME
+@endsection
+@section('content')
+<h1>MY MOVIES</h1>
+<div class="row">
+   @foreach ($movies as $movie)  {{--EACH MOVIE DB --}}
+    <div class="card" style="width: 18rem;">
+        <a href="{{route('movie', $movie->id)}}" class="text-decoration-none">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Title: {{ $movie->title}}</li>
+            <li class="list-group-item">OG Title: {{ $movie->original_title}}</li>
+            <li class="list-group-item">Nationality: {{ $movie->nationality}}</li>
+            <li class="list-group-item">Date: {{ $movie->date}}</li>
+            <li class="list-group-item">Vote: {{ $movie->vote}}</li>
+          </ul>
+        </a>
+      </div>
+    
+    @endforeach
+</div>        
+@endsection
+        
+    
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-         @vite('resources/js/app.js')
-    </head>
-    <body>
-
+@extends('layouts.app')
+@section('title')
+{{$single_movie->title}}
+@endsection
+@section('content')
         {{-- SINGLE MOVIE --}}
        <h1>{{$single_movie->title}}</h1>
        <h1><a href="{{route('home')}}" class="text-decoration-none"><h1><span class="badge bg-primary">HOME</span></h1></a></h1>
             
-    </body>
-</html>
+@endsection
